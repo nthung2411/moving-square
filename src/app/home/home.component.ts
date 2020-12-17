2cfd27983b40b3d1a62124de46c0e826ecfb97d2
Robert Hung Nguyen <nthung2411@gmail.com>
2020-12-17 23:27:14 +0700

    fix first time no animation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import {
+  AfterViewInit,
   Component,
   ElementRef,
   ViewChild
@@ -9,22 +10,37 @@ import {
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-
+export class HomeComponent implements AfterViewInit {
   public transitionTime: number = 200;
   @ViewChild('theBox') theBox: ElementRef | undefined;
 
+  ngAfterViewInit(): void {
+    const transitionParams = {
+      x: 0,
+      y: 0
+    }
+    this.doMoveBoxAnimation(transitionParams);
+  }
+
   public moveBoxToHere(event: any) {
-    const animationStyle = this.generateAnimationStyle(event);
+    const transitionParams = {
+      x: event.x,
+      y: event.y - 72
+    }
+    this.doMoveBoxAnimation(transitionParams);
+  }
+
+  private doMoveBoxAnimation(transitionParams: { x: number; y: number; }) {
+    const animationStyle = this.generateAnimationStyle(transitionParams);
     this.getTheBoxElement().setAttribute('style', animationStyle);
   }
 
-  private generateAnimationStyle(event: any): string {
+  private generateAnimationStyle(params: { x: number, y: number }): string {
     const transition =
       `transition: top ${this.transitionTime}ms ease-in-out, ` +
       `left ${this.transitionTime}ms ease-in-out;`;
-    const left = `left:${event.x}px;`;
-    const right = `top:${event.y - 72}px;`;
+    const left = `left:${params.x}px;`;
+    const right = `top:${params.y}px;`;
     return `${transition} ${left} ${right}`;
   }
 
