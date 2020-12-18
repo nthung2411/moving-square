ef53222bc43d53dbbc7a346ec31715bd897958aa
Robert Hung Nguyen <hung.nguyentan@axonactive.com>
2020-12-18 14:08:09 +0700

    code grooming

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,32 +4,34 @@ import {
   ElementRef,
   ViewChild
 } from '@angular/core';
+const offsetLeft = 8;
+const offSetTop = 72;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements AfterViewInit {
-  public transitionTime: number = 200;
+  public transitionTime = 200;
   @ViewChild('theBox') theBox: ElementRef | undefined;
 
   ngAfterViewInit(): void {
     const transitionParams = {
       x: 0,
       y: 0
-    }
+    };
     this.doMoveBoxAnimation(transitionParams);
   }
 
-  public moveBoxToHere(event: any) {
+  public moveBoxToHere(event: any): void {
     const transitionParams = {
-      x: event.x - 8,
-      y: event.y - 72
-    }
+      x: event.x - offsetLeft,
+      y: event.y - offSetTop
+    };
     this.doMoveBoxAnimation(transitionParams);
   }
 
-  private doMoveBoxAnimation(transitionParams: { x: number; y: number; }) {
+  private doMoveBoxAnimation(transitionParams: { x: number; y: number; }): void {
     const animationStyle = this.generateAnimationStyle(transitionParams);
     this.getTheBoxElement().setAttribute('style', animationStyle);
   }
@@ -38,12 +40,12 @@ export class HomeComponent implements AfterViewInit {
     const transition =
       `transition: top ${this.transitionTime}ms ease-in-out, ` +
       `left ${this.transitionTime}ms ease-in-out;`;
-    const left = `left:${params.x}px;`;
-    const right = `top:${params.y}px;`;
+    const left = `left: ${params.x}px;`;
+    const right = `top: ${params.y}px;`;
     return `${transition} ${left} ${right}`;
   }
 
-  private getTheBoxElement() {
+  private getTheBoxElement(): any {
     return this.theBox?.nativeElement;
   }
 }
